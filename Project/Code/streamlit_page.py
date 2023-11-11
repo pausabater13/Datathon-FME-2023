b4da9f6c23c5388ec43eea87d7655f24217a22ae
Pau Sabater Nácher <psabnac@pc-142-203.customer.ask4.lan>
2023-11-11 16:54:51 +0100

    streamlit page update

--- Project/Code/streamlit_page.py
+++ Project/Code/streamlit_page.py
@@ -2,44 +2,53 @@ import streamlit as st
 import pandas as pd
 import os
 
+st.set_page_config(
+    page_icon=":chart_with_upwards_trend:",
+    layout="wide",  # Puedes cambiar a "centered" o "wide"
+    initial_sidebar_state="expanded",  # Puedes cambiar a "collapsed"expanded
+    )
+
 # Load the dataset
 # Replace 'your_dataset.csv' with the actual path to your dataset
 #df = pd.read_csv('your_dataset.csv')
 
-
 # Streamlit web app
 def main():
-    st.title("Clothing Image Viewer")
+    st.title("Fashion Compatibility Challenge - Datathon 2023")
 
     col1, col2, col3 = st.columns(3)
 
     # Display input boxes for each parameter
-    cod_modelo_color = col1.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
-    cod_color_code = col1.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
-    des_color_specification_esp = col1.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
-    des_agrup_color_eng = col1.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
+    cod_modelo_color = col1.text_input("Identifier for fashion product based on model & color.", "")
+    num_clothes_per_outfit = col2.text_input("Number of clothes per outfit. Default: arbitrary.", "")
 
-    des_sex = col2.selectbox("cod_modelo_color",("Unisex", "Female", "Male"))
-    des_age = col2.selectbox("cod_modelo_color",('Kids', 'Adult'))
-    des_line = col2.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
-    des_fabric = col2.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
+    image_path = os.path.join('..','Data', 'mango_logo.png')
+    col3.image(image_path, use_column_width=True)#caption='Generated Image',
 
-    des_product_category = col3.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
-    des_product_aggregated_family = col3.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
-    des_product_family = col3.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
-    des_product_type = col3.selectbox("cod_modelo_color",('Email', 'Home phone', 'Mobile phone'))
+    col4, col5 = st.columns(2)
 
     # Submit button
-    if st.button("Submit"):
-        image_path = os.path.join('..','Data', 'mango_logo.png')
-    
-        if image_path:
-            st.image(image_path, caption='Generated Image', use_column_width=True)
+    if col1.button("Submit"):
+        logo_path = os.path.join('../../datathon-2023-fashion-compatibility-main/datathon/dataset/images', cod_modelo_color.replace("-","_")+".jpg")
+        if logo_path:
+            col4.image(logo_path,  use_column_width=True) #'''caption='Generated Image','''
         else:
-            st.warning("No matching image found.")
-    else:
-        image_path = os.path.join('..','Data', 'mango_logo.png')
-        st.image(image_path, caption='Generated Image', use_column_width=True)
+            col4.warning("No matching logo_image found.")
+    
+    # Add content to the new columns
+    col4.write("Column 4 content")
+    col4_width_css = """
+        <style>
+            div[data-testid="stHorizontalBlock"] > div:first-child {
+                flex: 0 0 33%;
+                max-width: 33%;
+            }
+        </style>
+    """
+    col4.markdown(col4_width_css, unsafe_allow_html=True)
+
+    col5.write("Column 5 content")
+    
 
 if __name__ == '__main__':
     main()
