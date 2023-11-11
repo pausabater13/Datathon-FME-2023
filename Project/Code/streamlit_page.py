824f94ff9f9fef27d965cee7e44d2b2f40891cb7
Pau Sabater Nácher <psabnac@pc-142-203.customer.ask4.lan>
2023-11-11 18:13:48 +0100

    approach decision tree

--- Project/Code/streamlit_page.py
+++ Project/Code/streamlit_page.py
@@ -16,38 +16,30 @@ st.set_page_config(
 def main():
     st.title("Fashion Compatibility Challenge - Datathon 2023")
 
-    col1, col2, col3 = st.columns(3)
+    col1,col3, col2 = st.columns(3)
 
     # Display input boxes for each parameter
-    cod_modelo_color = col1.text_input("Identifier for fashion product based on model & color.", "")
-    num_clothes_per_outfit = col2.text_input("Number of clothes per outfit. Default: arbitrary.", "")
 
     image_path = os.path.join('..','Data', 'mango_logo.png')
-    col3.image(image_path, use_column_width=True)#caption='Generated Image',
+    col1.image(image_path, use_column_width=True)#caption='Generated Image',
+    cod_modelo_color = col1.text_input("Identifier for fashion product based on model & color.", "")
+    num_clothes_per_outfit = col1.text_input("Number of clothes per outfit. Default: arbitrary.", "")
 
-    col4, col5 = st.columns(2)
+    col3, col4 = st.columns(2)
 
     # Submit button
     if col1.button("Submit"):
         logo_path = os.path.join('../../datathon-2023-fashion-compatibility-main/datathon/dataset/images', cod_modelo_color.replace("-","_")+".jpg")
         if logo_path:
-            col4.image(logo_path,  use_column_width=True) #'''caption='Generated Image','''
+            col2.image(logo_path,  use_column_width="auto") #'''caption='Generated Image','''
         else:
-            col4.warning("No matching logo_image found.")
+            col2.warning("No matching logo_image found.")
     
     # Add content to the new columns
-    col4.write("Column 4 content")
-    col4_width_css = """
-        <style>
-            div[data-testid="stHorizontalBlock"] > div:first-child {
-                flex: 0 0 33%;
-                max-width: 33%;
-            }
-        </style>
-    """
-    col4.markdown(col4_width_css, unsafe_allow_html=True)
+    col3.write("Column 4 content")
+    col4.write("Column 5 content")
 
-    col5.write("Column 5 content")
+    
     
 
 if __name__ == '__main__':
